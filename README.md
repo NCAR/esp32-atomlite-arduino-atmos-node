0f99a6d196f941e12d2c025fe628b35970f841c9
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-09-20 17:05:21 -0600

    Compile atmos-node sketch via GitHub CI (#3)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # IoTwx AtmosNode
+
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/NCAR/esp32-atomlite-arduino-atmos-node/Build?logo=github&style=for-the-badge)](https://github.com/NCAR/esp32-atomlite-arduino-atmos-node/actions)
+
 Arduino implementation for IoTwx ATMOS node.
--- esp32-atomlite-arduino-atmos-node.ino
+++ esp32-atomlite-arduino-atmos-node.ino
@@ -5,11 +5,11 @@
     the Grove bme280, bme680 and veml6070.
 
     ===
-    This code sleeps for 60s, wakes up, takes a measurement, 
+    This code sleeps for 60s, wakes up, takes a measurement,
     transmits and then goes back to sleep.
 
     copyright (c) 2020 keith maull
-    Website    : 
+    Website    :
     Author     : kmaull-ucar
     Create Time:
     Change Log :
@@ -17,13 +17,13 @@
 #include <WiFi.h>
 #include <ArduinoJson.h>
 #include "SPIFFS.h"
-#include "Seeed_VEML6070.h" /// https://github.com/Seeed-Studio/Seeed_VEML6070 
+#include "Seeed_VEML6070.h" /// https://github.com/Seeed-Studio/Seeed_VEML6070
 #include "Seeed_BME280.h"   /// https://github.com/seeedstudio/Grove_BME280
 #include "seeed_bme680.h"   /// https://github.com/seeedstudio/Seeed_BME680
 #include "IoTwx.h"          /// https://github.com/ncar/esp32-atomlite-arduino-iotwx
 
 #define BMEX80_IIC_ADDR   uint8_t(0x76)
-#define VEMLXX70_IIC_ADDR uint8_t(0x38) 
+#define VEMLXX70_IIC_ADDR uint8_t(0x38)
 
 IoTwx         node;
 Seeed_BME680  bme680(BMEX80_IIC_ADDR);
@@ -35,14 +35,14 @@ bool          bme680_attached   = false;
 bool          bme280_attached   = false;
 bool          veml6070_attached = false;
 char*         sensor;
-char*         topic; 
-int           timezone;    
-int           reset_interval; 
-int           publish_interval; 
-int           max_frequency     = 80;    
+char*         topic;
+int           timezone;
+int           reset_interval;
+int           publish_interval;
+int           max_frequency     = 80;
 
 
-void publish_bme280_measurements() {   
+void publish_bme280_measurements() {
   float humidity    = sensor_bme280.getHumidity();
   float temperature = sensor_bme280.getTemperature();
   float pressure    = sensor_bme280.getPressure();
@@ -63,7 +63,7 @@ void publish_bme280_measurements() {
 }
 
 
-void publish_bme680_measurements() {  
+void publish_bme680_measurements() {
   if (bme680.read_sensor_data()) {
     blink_led(LED_FAIL, LED_FAST);
     return;
@@ -76,16 +76,16 @@ void publish_bme680_measurements() {
     char s[strlen(sensor)+64];
 
     strcpy(s, sensor); strcat(s, "/bme680/humidity");
-    node.publishMQTTMeasurement(topic, s, humidity, 0); 
+    node.publishMQTTMeasurement(topic, s, humidity, 0);
 
     strcpy(s, sensor); strcat(s, "/bme680/temperature");
-    node.publishMQTTMeasurement(topic, s, temperature, 0); 
-    
+    node.publishMQTTMeasurement(topic, s, temperature, 0);
+
     strcpy(s, sensor); strcat(s, "/bme680/pressure");
-    node.publishMQTTMeasurement(topic, s, pressure, 0); 
+    node.publishMQTTMeasurement(topic, s, pressure, 0);
 
     strcpy(s, sensor); strcat(s, "/bme680/gas");
-    node.publishMQTTMeasurement(topic, s, gas, 0); 
+    node.publishMQTTMeasurement(topic, s, gas, 0);
   }
 }
 
@@ -96,12 +96,12 @@ err_t publish_veml6070_measurements() {
 
     sensor_veml6070.wait_for_ready();
     CHECK_RESULT(ret, sensor_veml6070.read_step(step));
-    
+
     char s[strlen(sensor)+64];
 
     strcpy(s, sensor); strcat(s, "/veml6070/uv");
     node.publishMQTTMeasurement(topic, s, step, 0);
-    
+
     return ret;
 }
 
@@ -114,19 +114,19 @@ void setup() {
   String                    mac = String((uint32_t)ESP.getEfuseMac(), HEX);
 
   strcpy(uuid, "ESP32P_AtomLite_"); strcat(uuid, (const char*) mac.c_str());
-  
+
   Serial.begin(57600);
   Serial.println("[] This is the IoTwx AtmosNode.  Initializing ...");
-  
+
   start_millis              = millis();
   init_led(); // set up AtomLite LED
 
   node = IoTwx(wait_for_bluetooth_config(uuid, millis(), 90)); // initializes config.json
   if (node.isConfigured())
-  {  
+  {
     // initialize the I2C bus
     Wire.begin(26, 32, 10000);
-  
+
     // check for i2c device connectivity
     while(!i2c_device_connected) {
       // check for BMEx80 device
@@ -136,7 +136,7 @@ void setup() {
         blink_led(LED_FAIL, LED_FAST);
         delay(5000);
       } else i2c_device_connected = true;
-  
+
       // check for vemlxx70 device
       Wire.beginTransmission((uint8_t)VEMLXX70_IIC_ADDR);
       if (Wire.endTransmission() != 0) {
@@ -145,10 +145,10 @@ void setup() {
         delay(5000);
       } else i2c_device_connected = true;
     }
-    
+
     // initiliaze the BME680 sensor
     if (!bme680.init()) {
-      Serial.println("[error]: Device BME680 initialization failed.");    
+      Serial.println("[error]: Device BME680 initialization failed.");
       blink_led(LED_FAIL, LED_FAST);
     }
     else {
@@ -156,7 +156,7 @@ void setup() {
       blink_led(LED_OK, LED_SLOW);
       bme680_attached = true;
     }
-  
+
     // NOTE: BUG in init() still returns 1 when the device is not connected!
     if (bme680_attached || !sensor_bme280.init()) {
       Serial.println("[error]: Device BME280 initialization failed.");
@@ -171,10 +171,10 @@ void setup() {
       } else {
         Serial.println("[info]: Device BME280 initialization OK");
         //blink_led(LED_OK, LED_SLOW);
-        bme280_attached = true;    
+        bme280_attached = true;
       }
     }
-  
+
     delay(1000);
     if (sensor_veml6070.init() != -2) {
       Serial.println("[error]: Device VEML6070 initialization failed.");
@@ -183,7 +183,7 @@ void setup() {
     else {
       Serial.println("[]: Device VEML6070 initialization OK");
       blink_led(LED_OK, LED_SLOW);
-      veml6070_attached = true;    
+      veml6070_attached = true;
     }
 
     Serial.println("[] deserializing to JSON");
@@ -194,16 +194,16 @@ void setup() {
     Serial.println("[] reading from JSON doc (SPIFFS)");
     timezone         = atoi((const char*)doc["iotwx_timezone"]);
     sensor           = strdup((const char*)doc["iotwx_sensor"]);
-    topic            = strdup((const char*)doc["iotwx_topic"]);  
-    reset_interval   = 1000*60*atoi((const char*)doc["iotwx_reset_interval"]);  
-    publish_interval = atoi((const char*)doc["iotwx_publish_interval"]);  
+    topic            = strdup((const char*)doc["iotwx_topic"]);
+    reset_interval   = 1000*60*atoi((const char*)doc["iotwx_reset_interval"]);
+    publish_interval = atoi((const char*)doc["iotwx_publish_interval"]);
     max_frequency    = atoi((const char*)doc["iotwx_max_frequency"]);
 
     // begin shutdown sequence, downthrottle, shutdown wifi and BT
-    btStop(); Serial.println("[] BT disconnected for power reduction");  
-    setCpuFrequencyMhz(max_frequency); Serial.println(); Serial.print("[] CPU downthrottled to "); Serial.print(max_frequency); Serial.println("Mhz for power reduction");  
-    WiFi.mode(WIFI_OFF); Serial.println("[] Wifi shut off for power reduction");  
-  } else 
+    btStop(); Serial.println("[] BT disconnected for power reduction");
+    setCpuFrequencyMhz(max_frequency); Serial.println(); Serial.print("[] CPU downthrottled to "); Serial.print(max_frequency); Serial.println("Mhz for power reduction");
+    WiFi.mode(WIFI_OFF); Serial.println("[] Wifi shut off for power reduction");
+  } else
       Serial.println("[] halting: configuration corrupt");
 }
 
@@ -213,19 +213,19 @@ void loop() {
     Serial.println("[info]: measuring");
 
     last_millis = millis();
-    
+
     node.establishCommunications();
     if (bme680_attached) publish_bme680_measurements();
     if (bme280_attached) publish_bme280_measurements();
     if (veml6070_attached) publish_veml6070_measurements();
-        
+
     // Configure the timer to wake us up!
     delay(1000);
   }
 
-  if ( millis() - start_millis > reset_interval ) esp_restart(); 
+  if ( millis() - start_millis > reset_interval ) esp_restart();
 
   Serial.println("[info]: sleeping");
   esp_sleep_enable_timer_wakeup(publish_interval * 60L * 1000000L);
-  esp_light_sleep_start();     
+  esp_light_sleep_start();
 }
